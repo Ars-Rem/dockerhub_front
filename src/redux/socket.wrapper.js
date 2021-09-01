428ca20fc93e11b30c4c13c5b2bae2b561f0cf09
Ars-Rem <1800gsm@gmail.com>
2021-09-01 18:37:06 +0300

    up

--- src/redux/socket.wrapper.js
+++ src/redux/socket.wrapper.js
@@ -5,7 +5,7 @@ import store from './createStore';
 let socket = null;
 
 export const connectSocket = () => {
-  socket = io('http://localhost:8085');
+  socket = io('http://localhost:6525');
   socket.on('new bleat', (data) => {
     store.dispatch(AddMessageRequest(JSON.parse(data)));
   });
