03e86fc4564fe6e37c6a290b9cdc95c28fb3251b
Ars-Rem <1800gsm@gmail.com>
2021-09-07 12:31:42 +0300

    n

--- src/redux/socket.wrapper.js
+++ src/redux/socket.wrapper.js
@@ -5,7 +5,7 @@ import store from './createStore';
 let socket = null;
 
 export const connectSocket = () => {
-  socket = io('194.44.111.70:4730');
+  socket = io('194.44.111.70:4730/blog');
   socket.on('new bleat', (data) => {
     store.dispatch(AddMessageRequest(JSON.parse(data)));
   });
